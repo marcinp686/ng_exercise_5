6c8e5bc2e73160516c05ad7c96766f5bd7dc9bbb
Marcin Plichta <marcinp689@gmail.com>
2022-11-30 21:53:54 +0100

    Added FakestoreService

---
+++ src/app/services/fakestore.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Cart } from '../models/cart.model';
+import { Category } from '../models/category.model';
+import { Product } from '../models/product.model';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FakestoreService {
+
+  private productsUrl : string = 'https://fakestoreapi.com';
+
+  constructor(private http: HttpClient) { }
+
+  // Products data
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productsUrl+'/products');
+  }
+
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(this.productsUrl+`/products/${id}`);
+  }
+  
+  createProduct(product: Product) : Observable<any> {  
+    return this.http.post(this.productsUrl, product);
+  }
+
+  // User data
+  getUser(id: number) : Observable<User> {
+    return this.http.get<User>(this.productsUrl+`/users/${id}`);
+  }
+
+  createUser(user: User) : Observable<any> {       
+    return this.http.post(this.productsUrl+'/users', user);
+  }
+
+  userLogin(user: User) : Observable<any> {
+    return this.http.post(this.productsUrl+'/auth/login', user);
+  }
+
+  // Categories data
+  getCategories(): Observable<Category[]> {
+    return this.http.get<string[]>(this.productsUrl+'/products/categories')
+      .pipe(map( (categories: string[]) => categories.map(
+        (category: string) => {const res: Category = { name: category }; return res;})))
+  }
+
+  // Cart data
+  getCart(id: number) : Observable<Cart> {
+    return this.http.get<Cart>(this.productsUrl+`/carts/${id}`);
+  }
+
+}
